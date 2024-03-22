c9e3d229b52d1932f6b3198f143fb2d6ed611527
msi <msi@DESKTOP-DFQDG4E>
2024-03-22 15:06:58 +0300

    Add project files.

---
+++ WindowsFormsApp2/EmanetIslemleri.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class EmanetIslemleri : Form
+    {
+        public EmanetIslemleri()
+        {
+            InitializeComponent();
+        }
+
+        private void EmanetIslemleri_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 frm1 = new Form1();
+            frm1.Show();
+            this.Hide();
+        }
+    }
+}
